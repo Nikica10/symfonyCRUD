0e4adef232e7156c5340d074d811a9de9bdf8df0
Nikica10 <niktitanik81@gmail.com>
2019-01-18 19:49:00 +0100

    Add comments

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comments;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -57,16 +61,72 @@ class ArticleController extends Controller
      * @param $id
      * @return Response
      */
-    public function singleArticleAction($id)
+    public function singleArticleAction(Request $request, $id)
     {
 
         $post = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findOneBy(['id' => $id]);
 
+        // comments
+        $entityManager = $this->getDoctrine()->getManager();
+        $newComment = new Comments();
+
+
+
+        $form = $this->createFormBuilder($newComment)
+            ->add('title', TextType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('comment', TextareaType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create Comment',
+                'attr' => array(
+                    'class' => 'btn btn-primary'
+                )
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newComment = $form->getData()->setArticle($post);
+
+            $entityManager->persist($newComment);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Komentar je spremljen u bazu'
+            );
+
+        }
+
+        // display comments
+//        $comments = $this->getDoctrine()
+//            ->getRepository(Comments::class)
+//            ->findAll();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT c
+        FROM App\Entity\Comments c
+        WHERE c.article = :articleId'
+        )->setParameter('articleId', $id);
+
 
         return $this->render('Home/singleArticle.html.twig', array(
-            'post' => $post
+            'post' => $post,
+            'form' => $form->createView(),
+            'comments' => $query->execute()
         ));
     }
 }
---
+++ src/Entity/Comments.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $article;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+}
